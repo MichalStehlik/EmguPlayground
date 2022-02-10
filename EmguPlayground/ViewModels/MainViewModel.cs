32bb55eb2f4e49b1a0831cd4e9a88277db928f6a
Michal Stehlík <michal.stehlik@pslib.cz>
2022-02-10 11:18:31 +0100

    face detection

--- EmguPlayground/ViewModels/MainViewModel.cs
+++ EmguPlayground/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -33,7 +34,7 @@ namespace EmguPlayground.ViewModels
             GrayscaleCommand = new RelayCommand(
                 () =>
                 {
-                    Mat img = new Mat(Preview.Size, DepthType.Cv8U, 1);
+                    Mat img = new Mat();
                     CvInvoke.CvtColor(Preview, img, ColorConversion.Rgba2Gray);
                     Preview = img;
                     GrayscaleCommand?.RaiseCanExecuteChanged();
@@ -43,7 +44,7 @@ namespace EmguPlayground.ViewModels
             BlurCommand = new RelayCommand(
                 () =>
                 {
-                    Mat img = new Mat(Preview.Size, DepthType.Cv8U, 3);
+                    Mat img = new Mat();
                     CvInvoke.GaussianBlur(Preview, img, new System.Drawing.Size(5, 5), 1.5);
                     Preview = img;
                 },
@@ -52,7 +53,7 @@ namespace EmguPlayground.ViewModels
             CannyCommand = new RelayCommand(
                 () =>
                 {
-                    Mat img = new Mat(Preview.Size, DepthType.Cv8U, 3);
+                    Mat img = new Mat();
                     CvInvoke.Canny(Preview, img, 100, 200);
                     Preview = img;
                 },
@@ -61,8 +62,45 @@ namespace EmguPlayground.ViewModels
             FacesCommand = new RelayCommand(
                 () =>
                 {
-                    Mat img = new Mat(Preview.Size, DepthType.Cv8U, 3);
-                    CvInvoke.Canny(Preview, img, 100, 200);
+                    Mat img = Preview;
+                    var faceDetector = new CascadeClassifier("haarcascade_frontalface_default.xml");
+                    var faces = faceDetector.DetectMultiScale(img,1.1,3,new System.Drawing.Size(100,100));
+                    foreach (var face in faces)
+                    {
+                        CvInvoke.Rectangle(img, new Rectangle(face.Left, face.Top, face.Width, face.Height), new Rgb(0, 0, 255).MCvScalar);
+                    }
+                    Preview = img;
+                },
+                () => { return (!Preview.IsEmpty); }
+            );
+            NormalizeGrayscaleCommand = new RelayCommand(
+                () =>
+                {
+                    Mat gray = new Mat();
+                    if (Preview.NumberOfChannels == 3)
+                    {
+                        CvInvoke.CvtColor(Preview, gray, ColorConversion.Rgba2Gray);
+                    }
+                    else
+                    {
+                        gray = Preview;
+                    }
+                    CvInvoke.EqualizeHist(gray, gray);
+                    Preview = gray;
+                },
+                () => { return (!Preview.IsEmpty); }
+            );
+            RectangleCommand = new RelayCommand(
+                () =>
+                {
+
+                    Mat img = Preview;
+                    Random rng = new Random();
+                    int x = rng.Next(img.Width);
+                    int y = rng.Next(img.Height);
+                    int w = rng.Next(0,img.Width - x);
+                    int h = rng.Next(0,img.Height - y);
+                    CvInvoke.Rectangle(img, new Rectangle(x,y,w,h), new Rgb(0, 0, 255).MCvScalar);
                     Preview = img;
                 },
                 () => { return (!Preview.IsEmpty); }
@@ -78,6 +116,8 @@ namespace EmguPlayground.ViewModels
                     BlurCommand.RaiseCanExecuteChanged();
                     CannyCommand.RaiseCanExecuteChanged();
                     FacesCommand.RaiseCanExecuteChanged();
+                    NormalizeGrayscaleCommand.RaiseCanExecuteChanged();
+                    RectangleCommand.RaiseCanExecuteChanged();
                 },
                 () => { return true; }
             );
@@ -102,6 +142,8 @@ namespace EmguPlayground.ViewModels
         public RelayCommand CannyCommand { get; set; }
         public RelayCommand FacesCommand { get; set; }
         public RelayCommand CaptureCommand { get; set; }
+        public RelayCommand NormalizeGrayscaleCommand { get; set; }
+        public RelayCommand RectangleCommand { get; set; }
 
         public void LoadImage(string filename)
         {
@@ -112,6 +154,8 @@ namespace EmguPlayground.ViewModels
             BlurCommand.RaiseCanExecuteChanged();
             CannyCommand.RaiseCanExecuteChanged();
             FacesCommand.RaiseCanExecuteChanged();
+            NormalizeGrayscaleCommand.RaiseCanExecuteChanged();
+            RectangleCommand.RaiseCanExecuteChanged();
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
